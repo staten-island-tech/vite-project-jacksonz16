c1717271865db5ab2d4e9b02d9ada7947d126177
jacksonz16 <jacksonz2390@gmail.com>
2023-11-08 08:45:06 -0500

    1

---
+++ app.js
@@ -0,0 +1,10 @@
+function spaces( s,y,t){
+        let total = 0;
+        for(let i=0;i<=s;i++){
+            if (y[i]=="c"&&t[i]==="c"){
+                total++;
+            }
+        }
+    }
+    spaces(7,"CCCCCCC","C.C.C.C")
+    
