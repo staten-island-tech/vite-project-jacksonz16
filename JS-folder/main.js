a139aac53ce4d880ef24af2b286cbc5c54842da3
jacksonz16 <jacksonz2390@gmail.com>
2023-11-21 09:27:05 -0500

    1

--- JS-folder/main.js
+++ JS-folder/main.js
@@ -1,21 +1,7 @@
-import '../style-folder/style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import "../styles/style.css";
+import { menu } from "./menu";
+document.querySelector("#app").innerHTML = `
+  <h1>Hello?????</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+console.log(menu);
