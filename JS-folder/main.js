eb1e7aeb234c03aa1c99fcb45760a9e69a3e4bea
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2023-11-27 01:39:08 -0500

    1

--- JS-folder/main.js
+++ JS-folder/main.js
@@ -1,7 +1,95 @@
-import "../styles/style.css";
-import { menu } from "./menu";
-document.querySelector("#app").innerHTML = `
-  <h1>Hello?????</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
-console.log(menu);
+import '../css/style.css'
+import { DOMselectors } from './dom.js'
+import { evangelion } from './menu.js'
+
+function toggleColors() {
+    if (DOMselectors.body.classList.contains("light")) {
+        DOMselectors.body.classList.remove("light");
+        DOMselectors.body.classList.add("dark");
+    } else if (DOMselectors.body.classList.contains("dark")) {
+        DOMselectors.body.classList.remove("dark");
+        DOMselectors.body.classList.add("light");
+    };
+};
+DOMselectors.toggleButton.addEventListener("click", toggleColors);
+
+evangelion.forEach(merch => {
+    DOMselectors.menu.insertAdjacentHTML(
+        "beforeend",
+        `
+            <div class="menu-item">
+                <img src="${merch.image}" alt="" class="item-img">
+                <h2 class="item-name">${merch.name}</h2>
+            </div>
+        `
+    );
+});
+
+function filterAll() {
+    DOMselectors.menu.innerHTML = "";
+    evangelion.forEach(merch => {
+        DOMselectors.menu.insertAdjacentHTML(
+            "beforeend",
+            `
+                <div class="menu-item">
+                    <img src="${merch.image}" alt="" class="item-img">
+                    <h2 class="item-name">${merch.name}</h2>
+                </div>
+            `
+        );
+    });
+};
+DOMselectors.allFilter.addEventListener("click", filterAll);
+
+function filtershiniji() {
+    DOMselectors.menu.innerHTML = "";
+    const shiniji = evangelion.filter(merch => merch.shiniji === true);
+    shiniji.forEach(merch => {
+        DOMselectors.menu.insertAdjacentHTML(
+            "beforeend",
+            `
+                <div class="menu-item">
+                    <img src="${merch.image}" alt="" class="item-img">
+                    <h2 class="item-name">${merch.name}</h2>
+                </div>
+            `
+        );
+    });
+};
+DOMselectors.shinijiFilter.addEventListener("click", filterl);
+
+function filterasuka() {
+    DOMselectors.menu.innerHTML = "";
+    const asuka = evangelion.filter(merch => merch.side === true);
+    asuka.forEach(merch => {
+        DOMselectors.menu.insertAdjacentHTML(
+            "beforeend",
+            `
+                <div class="menu-item">
+                    <img src="${merch.image}" alt="" class="item-img">
+                    <h2 class="item-name">${merch.name}</h2>
+                </div>
+            `
+        );
+    });
+};
+DOMselectors.asukaFilter.addEventListener("click", filterasuka
+
+);
+
+function filterrei() {
+    DOMselectors.menu.innerHTML = "";
+    const reimerch = evangelion.filter(merch => merch.rei === true);
+    reimerch.forEach(merch => {
+        DOMselectors.menu.insertAdjacentHTML(
+            "beforeend",
+            `
+                <div class="menu-item">
+                    <img src="${merch.image}" alt="" class="item-img">
+                    <h2 class="item-name">${merch.name}</h2>
+                </div>
+            `
+        );
+    });
+};
+DOMselectors.reiFilter.addEventListener("click", filterrei);
