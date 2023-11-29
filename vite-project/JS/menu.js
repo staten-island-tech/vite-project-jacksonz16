7253b27185e6eefeea7b2f27822d8a1539041d4e
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2023-11-28 22:34:29 -0500

    done????

--- vite-project/JS/menu.js
+++ vite-project/JS/menu.js
@@ -2,12 +2,246 @@
 export const Merch = [
     {
         name: "REI DOLL",
-        image: '/nyc-school-lunch_bad-biscuit.png',
+        image: '/REI DOLL.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI DOLL2",
+        image: '/rei1.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI DOLL3",
+        image: '/Screenshot 2023-11-28 210429.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },   {
+        name: "REI DOLL4",
+        image: '/public/Screenshot 2023-11-28 210409.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI DOLL5",
+        image: '/Screenshot 2023-11-28 210344.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },{
+        name: "REI DOLL6",
+        // image: '/Screenshot 2023-11-28 210248.png'
+        image: '/Screenshot 2023-11-28 212808.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI DOLL7",
+        image: '/Screenshot 2023-11-28 210511.png',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI figure",
+        image: '/8bcf4751242f0f011666b10bb4ae990e.webp',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI figure 2",
+        image: '/6d2c044dc66628fb0e42f3a0dcce7226.webp',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI canned bread",
+        image: '/public/P1010917.JPG',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI UNIT00(v1)",
+        image: '/public/fhqfaww4sae51.jpg',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI UNIT00(v2)",
+        image: '/public/61EuCGNMu9L.jpg',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI UNIT00 body pillow",
+        image: '/foxtail-img-3682.jpg',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "REI body pillow",
+        image: '/S30152b88236747718dbbdd92245930d8p.jpg_640x640Q90.jpg_.webp',
+        Shiniji: false,
+        REI: true,
+        Asuka: false,
+    
+    },
+    {
+        name: "ASUKA DOLL",
+        image: '/Screenshot 2023-11-28 215045.png',
         Shiniji: false,
         REI: false,
         Asuka: true,
     
     },
-   
-  
+    {
+        name: "ASUKA DOLL 2",
+        image: '/s-l1600.jpg',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA DOLL 3",
+        image: '/IMG_9623__89626.jpg',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA figure",
+        image: '/4981932514161_figure-asuka-shikinami-langley-eva2020-ver-evangelion-3-0-1-0-thrice-upon-a-time-altg.jpg',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA figure2",
+        image: '/51FC2gDCRHL.jpg',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA UNIT 002 1",
+        image: '/513fxap0L6L.webp',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA UNIT 002 2",
+        image: '/51gbkO3oTcL.webp',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "ASUKA body pillow",
+        image: '/61ORosvpmyL.jpg',
+        Shiniji: false,
+        REI: false,
+        Asuka: true,
+    
+    },
+    {
+        name: "Shinji doll",
+        image: '/s-l1200.webp',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+    {
+        name: "Shinji doll2",
+        image: '/images.jpg',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+    {
+        name: "Shinji doll3",
+        image: '/images (1).jpg',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+    {
+        name: "Shinji doll4",
+        image: '/55881.jpg',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+    {
+        name: "Shinji figure",
+        image: '/b29eb54ba3ede9156d96c54be640ab07.webp',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+    {
+        name: "Shinji figure2",
+        image: '/51IlIcru1oL.jpg',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },  {
+        name: "Shinji unit 001",
+        image: '/Bandai-Eva-Theater-Edition-03-Neon-Genesis-Evangelion-Eva-Evangelion-01-Awakening-Assembly-Model-Kids-Toys-1.jpg',
+        Shiniji: true,
+        REI: false,
+        Asuka: false,
+    
+    },
+ {
+    name: "Shinji unit 001 body pillow",
+    image: '/il_1080xN.3632519723_8vfs.avif',
+    Shiniji: true,
+    REI: false,
+    Asuka: false,
+ },
+ {
+    name: "Shinji body pillow",
+    image: '/Screenshot 2023-11-28 222019.png',
+    Shiniji: true,
+    REI: false,
+    Asuka: false,
+ },
 ]
